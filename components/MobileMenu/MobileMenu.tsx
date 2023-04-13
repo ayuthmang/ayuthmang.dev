2850bfa43d71670db1bb608f73041bc0d68b46a1
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-04-13 20:21:26 +0700

    add mobile nav, migrate @reach to @radix-ui, upgrade deps

--- components/MobileMenu/MobileMenu.tsx
+++ components/MobileMenu/MobileMenu.tsx
@@ -1,20 +1,44 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
-import { DialogOverlay, DialogContent } from '@reach/dialog'
 import Icon from '../Icon'
+import * as Dialog from '@radix-ui/react-dialog'
+import UnstyledButton from 'components/UnstyledButton/UnstyledButton'
 
 type MobileMenuProps = {
   isOpen?: boolean
   onDismiss: () => void
 }
 
-const MobileMenu: React.FC<MobileMenuProps> = ({ isOpen = false, onDismiss }) => {
+const MobileMenu: React.FC<MobileMenuProps> = ({
+  isOpen = false,
+  onDismiss,
+}) => {
   return (
-    <Wrapper isOpen={isOpen} onDismiss={onDismiss}>
-      <InnerWrapper>
-        <Icon id="x" size={32} />
-      </InnerWrapper>
-    </Wrapper>
+    <Dialog.Root open={isOpen} onOpenChange={onDismiss}>
+      <Dialog.Portal>
+        <Overlay />
+        <Wrapper>
+          <Content>
+          <Backdrop />
+            <InnerWrapper>
+              <Dialog.Close asChild>
+                <CloseButton>
+                  <Icon id="x" size={32} />
+                </CloseButton>
+              </Dialog.Close>
+              <Filler />
+              <Nav>
+                <NavLink href="/">Home</NavLink>
+                <NavLink href="/">GitHub</NavLink>
+                <NavLink href="/">Medium</NavLink>
+                <NavLink href="/">Dev</NavLink>
+              </Nav>
+              <Filler />
+            </InnerWrapper>
+          </Content>
+        </Wrapper>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
 
@@ -36,7 +60,7 @@ const slideIn = keyframes`
   }
 `
 
-const Wrapper = styled(DialogOverlay)`
+const Overlay = styled(Dialog.Overlay)`
   position: fixed;
   top: 0;
   left: 0;
@@ -44,8 +68,6 @@ const Wrapper = styled(DialogOverlay)`
   bottom: 0;
   background: var(--color-backdrop);
   backdrop-filter: blur(5px);
-  display: flex;
-  justify-content: flex-end;
   animation: ${fadeIn} 500ms;
 `
 
@@ -55,7 +77,29 @@ const Backdrop = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background: peachpuff;
+  background: gray; // debug
+`
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  /* background: var(--color-backdrop,); */
+  display: flex;
+  justify-content: flex-end;
+  animation: ${fadeIn} 500ms;
+`
+
+const Content = styled.div`
+  --overfill: 16px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  height: 100%;
+  width: 300px;
+  padding: 24px 32px;
 `
 
 const InnerWrapper = styled.div`
@@ -64,7 +108,38 @@ const InnerWrapper = styled.div`
   height: 100%;
   animation: ${fadeIn} 600ms both;
   animation-delay: 400ms;
-  margin: 16px;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+
+const NavLink = styled.a`
+  font-size: 1.125rem;
+  font-weight: var(--font-weight-medium);
+  text-decoration: none;
+  text-transform: uppercase;
+  cursor: pointer;
+  color: inherit;
+  opacity: 0.75;
+  transition: opacity 0.2s ease-in-out 0s;
+
+  &:hover {
+    opacity: 1;
+  }
+`
+
+const Filler = styled.div`
+  flex: 1;
+`
+
+const CloseButton = styled(UnstyledButton)`
+  position: absolute;
+  top: 10px;
+  right: var(--overfill);
+  padding: 16px;
 `
 
 export default MobileMenu
