a0098c3eb3109ae6fd53b1157e038f578c5299ba
Ayuth Mangmesap <ayuth.mang@gmail.com>
2024-03-25 00:28:22 +0700

    refactor: layout, remove unused

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,21 +3,27 @@ import Footer from '~/components/Footer'
 import Header from '~/components/Header'
 import StyledComponentsRegistry from '~/components/StyledComponentsRegistry'
 import './globals.css'
+import React from 'react'
+import MaxWidthWrapper from '~/components/MaxWidthWrapper'
 
 export const metadata: Metadata = {
   title: 'Ayuth Mangmesap',
   description: 'My personal website',
 }
 
-export default function RootLayout({
-  children,
-}: {
+export type RootLayoutProps = {
   children: React.ReactNode
-}) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html>
-      <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      <body suppressHydrationWarning={true}>
+        <StyledComponentsRegistry>
+          <Header />
+          <MaxWidthWrapper>{children}</MaxWidthWrapper>
+          <Footer />
+        </StyledComponentsRegistry>
       </body>
     </html>
   )
