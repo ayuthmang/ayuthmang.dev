251cbce2ed08e120ea95441cb4980f1cf9732b5d
Ayuth Mangmesap <ayuth.mang@gmail.com>
2024-03-25 00:25:42 +0700

    refactor: add as const in constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,10 +2,10 @@ export const BREAKPOINTS = {
   phone: 600,
   tablet: 950,
   laptop: 1300,
-}
+} as const
 
 export const QUERIES = {
   phoneAndSmaller: `(max-width: ${BREAKPOINTS.phone / 16}rem)`,
   tabletAndSmaller: `(max-width: ${BREAKPOINTS.tablet / 16}rem)`,
   laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptop / 16}rem)`,
-}
+} as const
