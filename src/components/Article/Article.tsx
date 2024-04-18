8661c4bcd947c65daa70e7337d20a7796a89490f
Ayuth Mangmesap <ayuth.mang@gmail.com>
2024-04-18 22:15:25 +0700

    wip: fix article style

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -23,7 +23,10 @@ function Article({
     <Link href={link}>
       <Wrapper>
         <ImageWrapper>
-          <Image src={thumbnail} alt="Article's cover image" />
+          <Image
+            src={thumbnail}
+            alt="Article's cover image"
+          />
         </ImageWrapper>
         <Title>{title}</Title>
         {/* <Description dangerouslySetInnerHTML={{ __html: description }} /> */}
@@ -44,18 +47,22 @@ const Link = styled.a`
 
 const Image = styled.img`
   display: inline;
-  height: 15rem;
   width: 100%;
+  height: 100%;
   object-fit: cover;
   will-change: transform;
-  transition: transform 600ms, filter 1000ms;
+  transition:
+    transform 600ms,
+    filter 1000ms;
   filter: brightness(90%);
 
   @media (hover: hover) and (prefers-reduced-motion: no-preference) {
     ${Link}:hover &,
     ${Link}:focus & {
       transform: scale(1.1);
-      transition: transform 250ms, filter 400ms;
+      transition:
+        transform 250ms,
+        filter 400ms;
       filter: brightness(100%);
     }
   }
@@ -76,6 +83,8 @@ const Description = styled.p``
 
 const ImageWrapper = styled.div`
   position: relative;
+  height: 15rem;
+  width: 100%;
   border-radius: 16px;
   overflow: hidden;
 `
