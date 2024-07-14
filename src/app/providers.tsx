5d282d45e0afd96c89089435df7f268c57d6d1e0
Ayuth Mangmesap <ayuth.mang@gmail.com>
2024-07-14 19:11:00 +0700

    refactor: move related to providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,13 +3,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
+import StyledComponentsRegistry from '~/components/StyledComponentsRegistry'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       <ReactQueryDevtools />
     </QueryClientProvider>
   )
