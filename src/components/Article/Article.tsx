b6c2be8e75816ee23f2d22e5335e3809d76d1adc
Ayuth Mangmesap <ayuth.mang@gmail.com>
2025-03-31 17:14:28 +0700

    add hover border and refactor

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -13,15 +13,20 @@ export type ArticleProps = {
 
 function Article({ guid, title, categories, thumbnail, link }: ArticleProps) {
   return (
-    <a href={link} className="group text-inherit no-underline">
-      <article className="flex flex-col gap-3 rounded-lg">
+    <a
+      href={link}
+      className={cn(
+        'group rounded-lg text-inherit no-underline',
+        'border-transparent transition-colors duration-300 ease-in-out hover:rounded-xl hover:bg-gray-700',
+      )}
+    >
+      <article className="m-3 flex flex-col gap-3">
         <div className="relative h-60 w-full overflow-hidden rounded-2xl">
           {/* eslint-disable-next-line @next/next/no-img-element, @next/next/no-img-element */}
           <img
             className={cn(
               'inline h-full w-full object-cover brightness-90 transition-[transform,filter] duration-[600ms,1000ms] will-change-transform',
-              'group-hover:scale-110 group-hover:brightness-100',
-              'group-hover:brightness-100 group-hover:transition-[transform,filter] group-hover:duration-[250ms,400ms]',
+              'group-hover:scale-110 group-hover:brightness-100 group-hover:transition-[transform,filter] group-hover:duration-[250ms,400ms]',
             )}
             src={thumbnail}
             alt="Article's cover image"
