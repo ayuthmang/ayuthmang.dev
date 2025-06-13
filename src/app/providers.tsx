85a6997cc364a3fe1d840a187d95ca94573fe14b
Ayuth Mangmesap <ayuth.mang@gmail.com>
2025-06-13 23:13:20 +0700

    feat: light/dark theme toggle added ☀️🌝

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,13 +4,21 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import StyledComponentsRegistry from '@/components/styled-components-registry'
+import { ThemeProvider } from '../components/theme-provider'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>
-      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </ThemeProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   )
