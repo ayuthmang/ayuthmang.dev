635d7e653d20d297ab6cdc714a59f2dcf49f08a8
Ayuth Mangmesap <ayuth.mang@gmail.com>
2025-12-29 14:14:38 +0700

    chore: add files related to the upgrade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import { Suspense } from 'react'
 import ArticleGrid from '@/components/article-grid'
 import ArticleGridSkeleton from '@/components/article-grid-skeleton'
 
 function HomePage() {
   return (
-    <div className="my-4">
-      <React.Suspense fallback={<ArticleGridSkeleton rows={3} columns={3} />}>
+    <div className="my-8">
+      <Suspense fallback={<ArticleGridSkeleton rows={3} columns={3} />}>
         <ArticleGrid />
-      </React.Suspense>
+      </Suspense>
     </div>
   )
 }
