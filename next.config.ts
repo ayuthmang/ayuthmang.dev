0c4ef332d27db5d36868c7d75878ad0852e7d784
Ayuth Mangmesap <ayuth.mang@gmail.com>
2026-06-21 12:50:27 +0700

    chore: cleanup styled-components, unused icon dep, use utils for func

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  compiler: {
-    styledComponents: {
-      ssr: true,
-    },
-  },
   images: {
     remotePatterns: [
       new URL('https://*.medium.com/**'),
